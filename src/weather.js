95eee66d092a38defcf74602c171e848af96e8e3
Moksh <moksh8516@gmail.com>
2025-01-29 12:13:12 +0530

    update urls

--- src/weather.js
+++ src/weather.js
@@ -4,7 +4,7 @@ const searchBar = document.querySelector(".search-bar");
 const searchBtn = document.querySelector(".btn");
 const weatherReportImg = document.querySelector(".weather-report");
 async function checkweather(city) {
-    const response = await fetch(`http://localhost:5000/api/weather?city=${city}`);
+    const response = await fetch(`https://moksh-weatherapi.netlify.app/api/weather?city=${city}`);
     // console.log(response)
 
     if (response.status == 404) {
@@ -52,8 +52,3 @@ searchBtn.addEventListener('click', () => {
 })
 
 checkweather();
-
-// class API{
-//  apiKey () {'f8a63fbbbb4aace54ad5c7fad0e2495f';}
-//  baseURL () {'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
-// }}
